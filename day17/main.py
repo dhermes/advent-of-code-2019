e2d8bfd89d89f18255c999d497d4df8d24866f1d
Danny Hermes <daniel.j.hermes@gmail.com>
2019-12-16 22:29:54 -0800

    Noice.

--- day17/main.py
+++ day17/main.py
@@ -10,10 +10,422 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import collections
+import copy
+import operator
 import pathlib
+import uuid
+
+import networkx as nx
+import numpy as np
 
 
 HERE = pathlib.Path(__file__).resolve().parent
+OPCODES = {
+    1: ("ADD", 3),
+    2: ("MULTIPLY", 3),
+    3: ("INPUT", 1),
+    4: ("OUTPUT", 1),
+    5: ("JUMP-IF-TRUE", 2),
+    6: ("JUMP-IF-FALSE", 2),
+    7: ("LESS-THAN", 3),
+    8: ("EQUALS", 3),
+    9: ("ADJUST_BASE", 1),
+    99: ("HALT", 0),
+}
+POSITION_MODE = "0"
+IMMEDIATE_MODE = "1"
+RELATIVE_MODE = "2"
+ALL_MODES = set("012")
+NO_JUMP_JUMP_INDEX = uuid.uuid4()
+TERMINAL_JUMP_INDEX = uuid.uuid4()
+DEBUG = True
+SCAFFOLD = "#"
+OPEN_SPACE = "."
+VACUUM_ROBOT_UP = "^"
+VACUUM_ROBOT_DOWN = "v"
+VACUUM_ROBOT_LEFT = "<"
+VACUUM_ROBOT_RIGHT = ">"
+VACUUM_ROBOT_POSITIONS = (
+    VACUUM_ROBOT_UP,
+    VACUUM_ROBOT_DOWN,
+    VACUUM_ROBOT_LEFT,
+    VACUUM_ROBOT_RIGHT,
+)
+VACUUM_ROBOT_TUMBLING = "X"
+# Main: {A,B,C}...   at most 20 w/o newline
+# A: {L,R,0,...,9}... at most 20 w/o newline
+# B: {L,R,0,...,9}... at most 20 w/o newline
+# C: {L,R,0,...,9}... at most 20 w/o newline
+# continuous video feed: {y,n}
+
+
+def debug(value, **kwargs):
+    if not DEBUG:
+        return
+    print(value, **kwargs)
+
+
+class AdjustBase:
+    def __init__(self, value):
+        self.value = value
+
+
+def less_than_binary_op(value1, value2):
+    if value1 < value2:
+        to_store = 1
+    else:
+        to_store = 0
+
+    return to_store
+
+
+def equal_binary_op(value1, value2):
+    if value1 == value2:
+        to_store = 1
+    else:
+        to_store = 0
+
+    return to_store
+
+
+def get_value(mode, param, relative_base, program):
+    if mode == POSITION_MODE:
+        index = param
+        assert 0 <= index
+        return program[index]
+
+    if mode == IMMEDIATE_MODE:
+        return param
+
+    if mode == RELATIVE_MODE:
+        index = relative_base + param
+        assert 0 <= index
+        return program[index]
+
+    raise ValueError("Invalid mode", mode)
+
+
+def set_value(mode, param, to_store, relative_base, program):
+    if mode == POSITION_MODE:
+        index = param
+        assert 0 <= index
+        program[index] = to_store
+        return
+
+    if mode == RELATIVE_MODE:
+        index = relative_base + param
+        assert 0 <= index
+        program[index] = to_store
+        return
+
+    raise ValueError("Invalid mode", mode)
+
+
+def _do_binary_op(modes, params, relative_base, program, fn):
+    mode1, mode2, mode3 = modes
+    param1, param2, param3 = params
+    value1 = get_value(mode1, param1, relative_base, program)
+    value2 = get_value(mode2, param2, relative_base, program)
+
+    to_store = fn(value1, value2)
+    set_value(mode3, param3, to_store, relative_base, program)
+
+    return NO_JUMP_JUMP_INDEX
+
+
+def do_add(modes, params, relative_base, program):
+    return _do_binary_op(modes, params, relative_base, program, operator.add)
+
+
+def do_multiply(modes, params, relative_base, program):
+    return _do_binary_op(modes, params, relative_base, program, operator.mul)
+
+
+def do_input(modes, params, relative_base, program, std_input):
+    mode, = modes
+    param, = params
+
+    to_store = next(std_input)
+    set_value(mode, param, to_store, relative_base, program)
+
+    return NO_JUMP_JUMP_INDEX
+
+
+def do_output(modes, params, relative_base, program, std_output):
+    mode, = modes
+    param, = params
+
+    value = get_value(mode, param, relative_base, program)
+    std_output.append(value)
+
+    return NO_JUMP_JUMP_INDEX
+
+
+def _do_jump_unary_predicate(modes, params, relative_base, program, fn):
+    mode1, mode2 = modes
+    param1, param2 = params
+
+    value1 = get_value(mode1, param1, relative_base, program)
+    value2 = get_value(mode2, param2, relative_base, program)
+
+    if fn(value1):
+        return value2
+
+    return NO_JUMP_JUMP_INDEX
+
+
+def do_jump_if_true(modes, params, relative_base, program):
+    return _do_jump_unary_predicate(
+        modes, params, relative_base, program, operator.truth
+    )
+
+
+def do_jump_if_false(modes, params, relative_base, program):
+    return _do_jump_unary_predicate(
+        modes, params, relative_base, program, operator.not_
+    )
+
+
+def do_less_than(modes, params, relative_base, program):
+    return _do_binary_op(
+        modes, params, relative_base, program, less_than_binary_op
+    )
+
+
+def do_equal(modes, params, relative_base, program):
+    return _do_binary_op(
+        modes, params, relative_base, program, equal_binary_op
+    )
+
+
+def do_adjust_base(modes, params, relative_base, program):
+    mode, = modes
+    param, = params
+
+    value = get_value(mode, param, relative_base, program)
+    return AdjustBase(value)
+
+
+def do_halt():
+    return TERMINAL_JUMP_INDEX
+
+
+def next_instruction(index, program):
+    assert 0 <= index
+    op_code_with_extra = program[index]
+    assert op_code_with_extra >= 0
+
+    mode_as_int, op_code = divmod(op_code_with_extra, 100)
+    instruction, num_params = OPCODES[op_code]
+    next_index = index + 1 + num_params
+    if num_params == 0:
+        assert mode_as_int == 0
+        return instruction, (), (), next_index
+
+    mode_chars = str(mode_as_int).zfill(num_params)
+    assert len(mode_chars) == num_params, (mode_chars, num_params)
+    assert set(mode_chars) <= ALL_MODES
+    modes = tuple(reversed(mode_chars))
+
+    params = tuple(program[i] for i in range(index + 1, next_index))
+    assert len(params) == num_params  # No partial slice
+
+    return instruction, modes, params, next_index
+
+
+def execute_instruction(
+    instruction, modes, params, relative_base, program, std_input, std_output
+):
+    if instruction == "ADD":
+        return do_add(modes, params, relative_base, program)
+
+    if instruction == "MULTIPLY":
+        return do_multiply(modes, params, relative_base, program)
+
+    if instruction == "INPUT":
+        return do_input(modes, params, relative_base, program, std_input)
+
+    if instruction == "OUTPUT":
+        return do_output(modes, params, relative_base, program, std_output)
+
+    if instruction == "JUMP-IF-TRUE":
+        return do_jump_if_true(modes, params, relative_base, program)
+
+    if instruction == "JUMP-IF-FALSE":
+        return do_jump_if_false(modes, params, relative_base, program)
+
+    if instruction == "LESS-THAN":
+        return do_less_than(modes, params, relative_base, program)
+
+    if instruction == "EQUALS":
+        return do_equal(modes, params, relative_base, program)
+
+    if instruction == "ADJUST_BASE":
+        return do_adjust_base(modes, params, relative_base, program)
+
+    if instruction == "HALT":
+        return do_halt()
+
+    raise ValueError("Bad instruction", instruction, modes, params, program)
+
+
+class Intcode:
+    def __init__(self, program, std_input, std_output):
+        self.running_program = copy.deepcopy(program)
+        self.std_input = std_input
+        self.std_output = std_output
+
+        self.relative_base = 0
+        self.index = 0
+        self.done = False
+
+    def advance_one(self):
+        assert not self.done
+
+        instruction, modes, params, index = next_instruction(
+            self.index, self.running_program
+        )
+        jump_index = execute_instruction(
+            instruction,
+            modes,
+            params,
+            self.relative_base,
+            self.running_program,
+            self.std_input,
+            self.std_output,
+        )
+        if isinstance(jump_index, AdjustBase):
+            self.relative_base += jump_index.value
+        elif jump_index in (NO_JUMP_JUMP_INDEX, TERMINAL_JUMP_INDEX):
+            # Nothing to do here, all good.
+            pass
+        elif jump_index >= 0:
+            index = jump_index
+        else:
+            raise ValueError("Invalid jump index", jump_index)
+
+        self.index = index
+        self.done = jump_index == TERMINAL_JUMP_INDEX
+
+    def run(self):
+        while not self.done:
+            self.advance_one()
+
+
+def part1(program):
+    std_input_list = []
+    std_input = iter(std_input_list)
+    std_output = []
+    intcode = Intcode(program, std_input, std_output)
+    intcode.run()
+
+    display = "".join(chr(value) for value in std_output)
+    display = display.rstrip() + "\n"
+    with open(HERE / "part1_output.txt", "w") as file_obj:
+        file_obj.write(display)
+
+    rows = display.strip().split("\n")
+    assert set(len(row) for row in rows) == set([51])
+    grid = np.array([list(row) for row in rows])
+
+    g = nx.Graph()
+    rows, cols = grid.shape
+    match_rows, match_cols = np.where(grid == SCAFFOLD)
+    scaffold_intersections = set()
+    for row, col in zip(match_rows, match_cols):
+        all_neighors_scaffold = True
+        if row > 0 and grid[row - 1, col] == SCAFFOLD:
+            g.add_edge((row, col), (row - 1, col))
+        else:
+            all_neighors_scaffold = False
+
+        if row < rows - 1 and grid[row + 1, col] == SCAFFOLD:
+            g.add_edge((row, col), (row + 1, col))
+        else:
+            all_neighors_scaffold = False
+
+        if col > 0 and grid[row, col - 1] == SCAFFOLD:
+            g.add_edge((row, col), (row, col - 1))
+        else:
+            all_neighors_scaffold = False
+
+        if col < cols - 1 and grid[row, col + 1] == SCAFFOLD:
+            g.add_edge((row, col), (row, col + 1))
+        else:
+            all_neighors_scaffold = False
+
+        if all_neighors_scaffold:
+            scaffold_intersections.add((row, col))
+
+    print(sum(row * col for row, col in scaffold_intersections))
+
+    return g, grid
+
+
+class VacuumRobot:
+    def __init__(self, g, grid, direction, position):
+        self.std_input = []
+        self.std_output = []
+        self.index = 0
+
+        self.g = g
+        self.grid = grid
+        self.direction = direction
+        self.position = np.array(position)
+        self.visited = set()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        curr_index = self.index
+        self.index = curr_index + 1
+        return self.std_input[curr_index]
+
+    def append(self, value):
+        self.std_output.append(value)
+
+    def send_command(self, movement_fns):
+        num_fns = len(movement_fns)
+        partners = [","] * (num_fns - 1) + ["\n"]
+        assert len(partners) == num_fns
+        for movement_fn, partner in zip(movement_fns, partners):
+            assert movement_fn in ("A", "B", "C", "L", "R")
+            self.std_input.extend([ord(movement_fn), ord(partner)])
+
+
+def find_exact(grid, value):
+    match_rows, match_cols = np.where(grid == value)
+    if match_rows.size == 0:
+        assert match_cols.size == 0
+        return None
+
+    assert match_rows.size == match_cols.size == 1
+    return match_rows[0], match_cols[0]
+
+
+def part2(program, g, grid):
+    matches = []
+    for direction in VACUUM_ROBOT_POSITIONS:
+        match = find_exact(grid, direction)
+        if match is not None:
+            matches.append((direction, match))
+
+    assert len(matches) == 1
+    match = matches[0]
+    direction, robot_position = match
+
+    # Force the vacuum robot to wake up by changing the value in your ASCII
+    # program at address 0 from 1 to 2
+    assert program[0] == 1
+    program[0] = 2
+
+    robot = VacuumRobot(g, grid, direction, robot_position)
+    intcode = Intcode(program, robot, robot)
+    intcode.run()
+
+    print(robot.std_output)
 
 
 def main():
@@ -21,7 +433,12 @@ def main():
     with open(filename, "r") as file_obj:
         content = file_obj.read()
 
-    print(len(content))
+    program = collections.defaultdict(int)
+    for index, value in enumerate(content.strip().split(",")):
+        program[index] = int(value)
+
+    g, grid = part1(program)
+    part2(program, g, grid)
 
 
 if __name__ == "__main__":
---
+++ day17/simulate.py
@@ -0,0 +1,118 @@
+import pathlib
+
+import numpy as np
+
+
+HERE = pathlib.Path(__file__).resolve().parent
+SCAFFOLD = "#"
+OPEN_SPACE = "."
+VACUUM_ROBOT_UP = "^"
+VACUUM_ROBOT_DOWN = "v"
+VACUUM_ROBOT_LEFT = "<"
+VACUUM_ROBOT_RIGHT = ">"
+VACUUM_ROBOT_POSITIONS = {
+    VACUUM_ROBOT_UP: np.array([-1, 0]),  # Visual up is "down" in row
+    VACUUM_ROBOT_DOWN: np.array([1, 0]),  # Visual down is "up" in row
+    VACUUM_ROBOT_LEFT: np.array([0, -1]),  # Visual left is "left" in col
+    VACUUM_ROBOT_RIGHT: np.array([0, 1]),  # Visual right is "right" in col
+}
+POSITIONS_REVERSE = {tuple(v): k for k, v in VACUUM_ROBOT_POSITIONS.items()}
+TURNS = {"L": np.array([[0, -1], [1, 0]]), "R": np.array([[0, 1], [-1, 0]])}
+PAIRS = {
+    ("L", 6): "V",
+    ("L", 8): "W",
+    ("L", 10): "X",
+    ("R", 10): "Y",
+    ("R", 12): "Z",
+}
+
+
+def find_exact(grid, value):
+    match_rows, match_cols = np.where(grid == value)
+    if match_rows.size == 0:
+        assert match_cols.size == 0
+        return None
+
+    assert match_rows.size == match_cols.size == 1
+    return match_rows[0], match_cols[0]
+
+
+def display_grid(grid):
+    _, cols = grid.shape
+    print("-" * cols)
+    for row in grid:
+        print("".join(row))
+
+
+def _apply_steps(grid, value, direction, robot_position):
+    robot_char = POSITIONS_REVERSE[tuple(direction)]
+    assert value > 0
+    new_position = robot_position
+    row, col = robot_position
+    for _ in range(value):
+        new_position += direction
+        row, col = new_position
+        if grid[row, col] in VACUUM_ROBOT_POSITIONS:
+            grid[row, col] = SCAFFOLD
+        else:
+            if grid[row, col] != SCAFFOLD:
+                raise RuntimeError("Fell off the scaffold")
+            grid[row, col] = robot_char
+
+    return direction, new_position
+
+
+def apply_command(grid, command, direction, robot_position):
+    if isinstance(command, int):
+        return _apply_steps(grid, command, direction, robot_position)
+
+    turn = TURNS[command]
+    new_direction = turn.dot(direction)
+    robot_char = POSITIONS_REVERSE[tuple(new_direction)]
+    row, col = robot_position
+    grid[row, col] = robot_char
+    return new_direction, robot_position
+
+
+def main():
+    with open(HERE / "part1_output.txt", "r") as file_obj:
+        display = file_obj.read()
+
+    rows = display.strip().split("\n")
+    assert set(len(row) for row in rows) == set([51])
+    grid = np.array([list(row) for row in rows])
+    # 0. Location the robot
+    matches = []
+    for direction_char, direction in VACUUM_ROBOT_POSITIONS.items():
+        match = find_exact(grid, direction_char)
+        if match is not None:
+            matches.append((direction, match))
+    assert len(matches) == 1
+    match = matches[0]
+    direction, robot_position = match
+    robot_position = np.array(robot_position)
+    # V:L06
+    # W:L08
+    # X:L10
+    # Y:R10
+    # Z:R12
+    # Start moving
+    path = ("R", 12, "L", 6, "R", 12, "L", 8, "L", 6, "L", 10, "R", 12)
+    path += ("L", 6, "R", 12, "R", 12, "L", 10, "L", 6, "R", 10, "L", 8)
+    path += ("L", 6, "L", 10, "R", 12, "L", 10, "L", 6, "R", 10, "L", 8)
+    path += ("L", 6, "L", 10, "R", 12, "L", 10, "L", 6, "R", 10, "R", 12)
+    path += ("L", 6, "R", 12, "R", 12, "L", 10, "L", 6, "R", 10)
+    for command in path:
+        direction, robot_position = apply_command(
+            grid, command, direction, robot_position
+        )
+
+    display_grid(grid)
+
+    print("".join(PAIRS[pair] for pair in zip(path[::2], path[1::2])))
+    # ZVZWVXZVZZXVYWVXZXVYWVXZXVYZVZZXVY
+    # [ZVZ][WVX][ZVZ][ZXVY][WVX][ZXVY][WVX][ZXVY][ZVZ][ZXVY]
+
+
+if __name__ == "__main__":
+    main()
